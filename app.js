36c6d2b829bcebd75fb175dea0f0aab073dc09dc
marc <10080192+mhlgoossens@users.noreply.github.com>
2020-10-19 18:11:48 +0200

    add express server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = (process.env.PORT || 8080);
+
+// find the app name in package.json
+app.use(express.static(__dirname + '/dist/crypto-dashboard'));
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/crypto-dashboard/index.html'));
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
