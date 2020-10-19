6e53ac91032e9ae49d4595fe90c6d196a836f61e
marc <10080192+mhlgoossens@users.noreply.github.com>
2020-10-19 16:10:33 +0200

    create project starter files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { CurrencyOverviewComponent } from './currency-overview/currency-overview.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ContactComponent,
+    AboutComponent,
+    ProfileComponent,
+    PortfolioComponent,
+    CurrencyOverviewComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'currency-overview', component: CurrencyOverviewComponent }, 
+      { path: 'about', component: AboutComponent },
+      { path: 'portfolio', component: PortfolioComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'contact', component: ContactComponent }
+    ]),
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/currency-overview/currency-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-overview',
+  templateUrl: './currency-overview.component.html',
+  styleUrls: ['./currency-overview.component.scss']
+})
+export class CurrencyOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
